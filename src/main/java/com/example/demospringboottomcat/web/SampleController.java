1ccb7108352e7e1e8cff180d3f9ca327d4c31cf1
Raj <rajeshdasoar@gmail.com>
2020-01-14 17:08:42 +0000

    Added webjar swagger-ui module and loaded the resource on startup

--- src/main/java/com/example/demospringboottomcat/web/SampleController.java
+++ src/main/java/com/example/demospringboottomcat/web/SampleController.java
@@ -3,12 +3,19 @@ package com.example.demospringboottomcat.web;
 import com.example.demospringboottomcat.service.HelloWorldService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @RestController
-public class SampleController {
+@EnableWebMvc
+public class SampleController implements WebMvcConfigurer {
 
     @Autowired
 	private HelloWorldService helloWorldService;
@@ -19,5 +26,14 @@ public class SampleController {
     	return this.helloWorldService.getHelloMessage();
 	}
 
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		/*registry.addResourceHandler("/docs/index.html")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.24.3");*/
+
+		registry.addResourceHandler("/docs/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui/3.24.3/");
+	}
 
 }
